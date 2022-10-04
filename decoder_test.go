71908e142adb299090d63a53f29a0a230492a7c8
Egon Elbre <egonelbre@gmail.com>
2022-10-04 11:40:33 +0300

    add end-to-end decoder test

---
+++ decoder_test.go
@@ -0,0 +1,72 @@
+package opus
+
+import (
+	"bytes"
+	"errors"
+	"flag"
+	"io"
+	"os"
+	"sync"
+	"testing"
+
+	"github.com/pion/opus/pkg/oggreader"
+)
+
+var testoggfile = flag.String("oggfile", "", "ogg file for benchmark")
+
+var _testogg struct {
+	once sync.Once
+	err  error
+	data []byte
+}
+
+func loadTestOgg(tb testing.TB) []byte {
+	if *testoggfile == "" {
+		tb.Skip("-oggfile not specified")
+	}
+
+	_testogg.once.Do(func() {
+		_testogg.data, _testogg.err = os.ReadFile(*testoggfile)
+	})
+	if _testogg.err != nil {
+		tb.Fatal("unable to load -oggfile", _testogg.err)
+	}
+	return _testogg.data
+}
+
+func BenchmarkDecode(b *testing.B) {
+	data := loadTestOgg(b)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		benchmarkData(b, data)
+	}
+}
+
+func benchmarkData(b *testing.B, data []byte) {
+	var out [1920]byte
+	ogg, _, err := oggreader.NewWith(bytes.NewReader(data))
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	decoder := NewDecoder()
+	for {
+		segments, _, err := ogg.ParseNextPage()
+
+		if errors.Is(err, io.EOF) {
+			break
+		} else if bytes.HasPrefix(segments[0], []byte("OpusTags")) {
+			continue
+		}
+
+		if err != nil {
+			b.Fatal(err)
+		}
+
+		for i := range segments {
+			if _, _, err = decoder.Decode(segments[i], out[:]); err != nil {
+				b.Fatal(err)
+			}
+		}
+	}
+}
