7a5e94fa7a6611dc14004efa98cc6c1eb114fce5
Steffen Vogel <post@steffenvogel.de>
2023-05-05 16:34:25 +0200

    Make repo REUSE compliant

--- internal/rangecoding/rangecoding.go
+++ internal/rangecoding/rangecoding.go
@@ -1,2 +1,5 @@
+// SPDX-FileCopyrightText: 2023 The Pion community <https://pion.ly>
+// SPDX-License-Identifier: MIT
+
 // Package rangecoding provides a Range coder for the Opus bitstream
 package rangecoding
