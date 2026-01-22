ff7754f6cc37cd58f18bb8856db58159abfcf21e
Pion <59523206+pionbot@users.noreply.github.com>
2026-01-22 08:43:42 +0000

    Update CI configs to v0.11.37
    Update lint scripts and CI configs.

--- decoder.go
+++ decoder.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2023 The Pion community <https://pion.ly>
+// SPDX-FileCopyrightText: 2026 The Pion community <https://pion.ly>
 // SPDX-License-Identifier: MIT
 
 // Package opus provides a Opus Audio Codec RFC 6716 implementation
