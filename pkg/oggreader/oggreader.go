76b231fc6b3f8e93db15a5c8910f7de0fa93ec9e
jarj <j.rj00@hotmail.es>
2026-04-12 17:24:42 +0200

    Parse logical Ogg packets in oggreader

--- pkg/oggreader/oggreader.go
+++ pkg/oggreader/oggreader.go
@@ -11,20 +11,28 @@ import (
 )
 
 const (
+	pageHeaderTypeContinuedPacket   = 0x01
 	pageHeaderTypeBeginningOfStream = 0x02
 	pageHeaderSignature             = "OggS"
+	pageChecksumOffset              = 22
+	pageSegmentCountOffset          = 26
 
 	idPageSignature = "OpusHead"
 
-	pageHeaderLen       = 27
-	idPagePayloadLength = 19
+	pageHeaderLen           = 27
+	idPagePayloadLength     = 19
+	idPageStreamCountIndex  = 19
+	idPageCoupledCountIndex = 20
+	idPageMappingIndex      = 21
+
+	maxPageSegmentSize = 255
 )
 
 var (
 	errNilStream                 = errors.New("stream is nil")
 	errBadIDPageSignature        = errors.New("bad header signature")
 	errBadIDPageType             = errors.New("wrong header, expected beginning of stream")
-	errBadIDPageLength           = errors.New("payload for id page must be 19 bytes")
+	errBadIDPageLength           = errors.New("payload for id page has invalid length")
 	errBadIDPagePayloadSignature = errors.New("bad payload signature")
 	errShortPageHeader           = errors.New("not enough data for payload header")
 	errChecksumMismatch          = errors.New("expected and actual checksum do not match")
@@ -36,6 +44,8 @@ type OggReader struct {
 	bytesReadSuccesfully int64
 	checksumTable        *[256]uint32
 	doChecksum           bool
+	packetState          *oggPacketState
+	channelMapping       *OggChannelMapping
 }
 
 // OggHeader is the metadata from the first two pages
@@ -49,6 +59,14 @@ type OggHeader struct {
 	PreSkip    uint16
 	SampleRate uint32
 	Version    uint8
+}
+
+// OggChannelMapping carries extended multistream metadata from the Opus ID header.
+type OggChannelMapping struct {
+	StreamCount    uint8
+	CoupledCount   uint8
+	Mapping        []uint8
+	DemixingMatrix []int16
 }
 
 // OggPageHeader is the metadata for a Page
@@ -66,6 +84,19 @@ type OggPageHeader struct {
 	segmentsCount uint8
 }
 
+type oggPacket struct {
+	data   []byte
+	header *OggPageHeader
+}
+
+type oggPacketState struct {
+	packetQueue         []oggPacket
+	partialPacket       []byte
+	discardingPacket    bool
+	lastPacketPageIndex uint32
+	hasLastPacketPage   bool
+}
+
 // NewWith returns a new Ogg reader and Ogg header
 // with an io.Reader input.
 func NewWith(in io.Reader) (*OggReader, *OggHeader, error) {
@@ -81,6 +112,7 @@ func newWith(in io.Reader, doChecksum bool) (*OggReader, *OggHeader, error) {
 		stream:        in,
 		checksumTable: generateChecksumTable(),
 		doChecksum:    doChecksum,
+		packetState:   &oggPacketState{},
 	}
 
 	header, err := reader.readHeaders()
@@ -92,48 +124,96 @@ func newWith(in io.Reader, doChecksum bool) (*OggReader, *OggHeader, error) {
 }
 
 func (o *OggReader) readHeaders() (*OggHeader, error) {
-	segments, pageHeader, err := o.ParseNextPage()
+	packet, pageHeader, err := o.ParseNextPacket()
 	if err != nil {
 		return nil, err
 	}
 
-	header := &OggHeader{}
-	if string(pageHeader.sig[:]) != pageHeaderSignature {
-		return nil, errBadIDPageSignature
+	header, err := parseIDHeader(pageHeader, packet)
+	if err != nil {
+		return nil, err
 	}
 
-	if pageHeader.headerType != pageHeaderTypeBeginningOfStream {
-		return nil, errBadIDPageType
+	if err = o.parseChannelMapping(header, packet); err != nil {
+		return nil, err
 	}
-
-	if len(segments[0]) != idPagePayloadLength {
-		return nil, errBadIDPageLength
-	}
-
-	if s := string(segments[0][:8]); s != idPageSignature {
-		return nil, errBadIDPagePayloadSignature
-	}
-
-	header.Version = segments[0][8]
-	header.Channels = segments[0][9]
-	header.PreSkip = binary.LittleEndian.Uint16(segments[0][10:12])
-	header.SampleRate = binary.LittleEndian.Uint32(segments[0][12:16])
-	header.OutputGain = binary.LittleEndian.Uint16(segments[0][16:18])
-	header.ChannelMap = segments[0][18]
 
 	return header, nil
 }
 
+// ChannelMapping returns a copy of the parsed multistream channel mapping metadata.
+func (o *OggReader) ChannelMapping() OggChannelMapping {
+	if o.channelMapping == nil {
+		return OggChannelMapping{}
+	}
+
+	return OggChannelMapping{
+		StreamCount:    o.channelMapping.StreamCount,
+		CoupledCount:   o.channelMapping.CoupledCount,
+		Mapping:        append([]uint8(nil), o.channelMapping.Mapping...),
+		DemixingMatrix: append([]int16(nil), o.channelMapping.DemixingMatrix...),
+	}
+}
+
 // ParseNextPage reads from stream and returns Ogg page segments, header,
 // and an error if there is incomplete page data.
+//
+// ParseNextPage and ParseNextPacket are alternative read paths and should not
+// be mixed on the same OggReader.
 func (o *OggReader) ParseNextPage() ([][]byte, *OggPageHeader, error) {
+	segments, _, pageHeader, err := o.parseNextPageData()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	o.markPacketPageRead(pageHeader)
+
+	return segments, pageHeader, nil
+}
+
+// ParseNextPacket reads from stream and returns one logical Ogg packet.
+//
+// ParseNextPage and ParseNextPacket are alternative read paths and should not
+// be mixed on the same OggReader.
+func (o *OggReader) ParseNextPacket() ([]byte, *OggPageHeader, error) {
+	packetState := o.getPacketState()
+	if len(packetState.packetQueue) > 0 {
+		packet := packetState.packetQueue[0]
+		packetState.packetQueue = packetState.packetQueue[1:]
+
+		return packet.data, packet.header, nil
+	}
+
+	for {
+		segments, sizeBuffer, pageHeader, err := o.parseNextPageData()
+		if err != nil {
+			if errors.Is(err, io.EOF) && len(packetState.partialPacket) != 0 {
+				return nil, nil, io.ErrUnexpectedEOF
+			}
+
+			return nil, nil, err
+		}
+
+		if err = o.queuePagePackets(segments, sizeBuffer, pageHeader); err != nil {
+			return nil, nil, err
+		}
+		if len(packetState.packetQueue) > 0 {
+			packet := packetState.packetQueue[0]
+			packetState.packetQueue = packetState.packetQueue[1:]
+
+			return packet.data, packet.header, nil
+		}
+	}
+}
+
+func (o *OggReader) parseNextPageData() ([][]byte, []byte, *OggPageHeader, error) {
 	header := make([]byte, pageHeaderLen)
 
 	n, err := io.ReadFull(o.stream, header)
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, nil, err
 	} else if n < len(header) {
-		return nil, nil, errShortPageHeader
+		return nil, nil, nil, errShortPageHeader
 	}
 
 	pageHeader := &OggPageHeader{
@@ -145,11 +225,11 @@ func (o *OggReader) ParseNextPage() ([][]byte, *OggPageHeader, error) {
 	pageHeader.GranulePosition = binary.LittleEndian.Uint64(header[6 : 6+8])
 	pageHeader.serial = binary.LittleEndian.Uint32(header[14 : 14+4])
 	pageHeader.index = binary.LittleEndian.Uint32(header[18 : 18+4])
-	pageHeader.segmentsCount = header[26]
+	pageHeader.segmentsCount = header[pageSegmentCountOffset]
 
 	sizeBuffer := make([]byte, pageHeader.segmentsCount)
 	if _, err = io.ReadFull(o.stream, sizeBuffer); err != nil {
-		return nil, nil, err
+		return nil, nil, nil, err
 	}
 
 	segments := [][]byte{}
@@ -157,7 +237,7 @@ func (o *OggReader) ParseNextPage() ([][]byte, *OggPageHeader, error) {
 	for _, s := range sizeBuffer {
 		segment := make([]byte, int(s))
 		if _, err = io.ReadFull(o.stream, segment); err != nil {
-			return nil, nil, err
+			return nil, nil, nil, err
 		}
 
 		segments = append(segments, segment)
@@ -165,11 +245,49 @@ func (o *OggReader) ParseNextPage() ([][]byte, *OggPageHeader, error) {
 
 	if o.doChecksum {
 		if err = o.validateChecksum(header, sizeBuffer, segments); err != nil {
-			return nil, nil, err
+			return nil, nil, nil, err
 		}
 	}
 
-	return segments, pageHeader, nil
+	return segments, sizeBuffer, pageHeader, nil
+}
+
+func (o *OggReader) queuePagePackets(segments [][]byte, sizeBuffer []byte, pageHeader *OggPageHeader) error {
+	packetState := o.getPacketState()
+	if err := o.preparePacketQueue(pageHeader); err != nil {
+		return err
+	}
+
+	for i, segment := range segments {
+		segmentSize := sizeBuffer[i]
+		if packetState.discardingPacket {
+			packetState.finishDiscard(segmentSize)
+
+			continue
+		}
+
+		packetState.partialPacket = append(packetState.partialPacket, segment...)
+		if segmentSize == maxPageSegmentSize {
+			continue
+		}
+
+		o.enqueuePacket(pageHeader, packetState.partialPacket)
+		packetState.partialPacket = nil
+	}
+
+	o.markPacketPageRead(pageHeader)
+
+	return nil
+}
+
+func (o *OggReader) markPacketPageRead(pageHeader *OggPageHeader) {
+	if pageHeader.segmentsCount == 0 {
+		return
+	}
+
+	packetState := o.getPacketState()
+	packetState.lastPacketPageIndex = pageHeader.index
+	packetState.hasLastPacketPage = true
 }
 
 func (o *OggReader) validateChecksum(header, sizeBuffer []byte, segments [][]byte) error {
@@ -180,7 +298,7 @@ func (o *OggReader) validateChecksum(header, sizeBuffer []byte, segments [][]byt
 
 	for index := range header {
 		// Don't include expected checksum in our generation
-		if index > 21 && index < 26 {
+		if index >= pageChecksumOffset && index < pageSegmentCountOffset {
 			updateChecksum(0)
 
 			continue
@@ -198,7 +316,7 @@ func (o *OggReader) validateChecksum(header, sizeBuffer []byte, segments [][]byt
 		}
 	}
 
-	if binary.LittleEndian.Uint32(header[22:22+4]) != checksum {
+	if binary.LittleEndian.Uint32(header[pageChecksumOffset:pageChecksumOffset+4]) != checksum {
 		return errChecksumMismatch
 	}
 
@@ -230,4 +348,151 @@ func generateChecksumTable() *[256]uint32 {
 	}
 
 	return &table
+}
+
+func parseIDHeader(pageHeader *OggPageHeader, packet []byte) (*OggHeader, error) {
+	if string(pageHeader.sig[:]) != pageHeaderSignature {
+		return nil, errBadIDPageSignature
+	}
+
+	if pageHeader.headerType != pageHeaderTypeBeginningOfStream {
+		return nil, errBadIDPageType
+	}
+
+	if len(packet) < idPagePayloadLength {
+		return nil, errBadIDPageLength
+	}
+
+	if s := string(packet[:8]); s != idPageSignature {
+		return nil, errBadIDPagePayloadSignature
+	}
+
+	return &OggHeader{
+		Version:    packet[8],
+		Channels:   packet[9],
+		PreSkip:    binary.LittleEndian.Uint16(packet[10:12]),
+		SampleRate: binary.LittleEndian.Uint32(packet[12:16]),
+		OutputGain: binary.LittleEndian.Uint16(packet[16:18]),
+		ChannelMap: packet[18],
+	}, nil
+}
+
+func (o *OggReader) parseChannelMapping(header *OggHeader, packet []byte) error {
+	switch header.ChannelMap {
+	case 0:
+		if len(packet) != idPagePayloadLength {
+			return errBadIDPageLength
+		}
+		o.channelMapping = nil
+
+		return nil
+	case 1, 2, 255:
+		return o.parseStreamChannelMapping(header, packet)
+	case 3:
+		return o.parseDemixingChannelMapping(header, packet)
+	default:
+		o.channelMapping = nil
+
+		return nil
+	}
+}
+
+func (o *OggReader) parseStreamChannelMapping(header *OggHeader, packet []byte) error {
+	expectedLength := idPageMappingIndex + int(header.Channels)
+	if len(packet) != expectedLength {
+		return errBadIDPageLength
+	}
+
+	o.channelMapping = &OggChannelMapping{
+		StreamCount:  packet[idPageStreamCountIndex],
+		CoupledCount: packet[idPageCoupledCountIndex],
+		Mapping:      append([]uint8(nil), packet[idPageMappingIndex:]...),
+	}
+
+	return nil
+}
+
+func (o *OggReader) parseDemixingChannelMapping(header *OggHeader, packet []byte) error {
+	if len(packet) < idPageMappingIndex {
+		return errBadIDPageLength
+	}
+
+	streamCount := packet[idPageStreamCountIndex]
+	coupledCount := packet[idPageCoupledCountIndex]
+	decodedChannels := int(streamCount) + int(coupledCount)
+	expectedLength := idPageMappingIndex + (2 * int(header.Channels) * decodedChannels)
+	if len(packet) != expectedLength {
+		return errBadIDPageLength
+	}
+
+	o.channelMapping = &OggChannelMapping{
+		StreamCount:    streamCount,
+		CoupledCount:   coupledCount,
+		DemixingMatrix: parseDemixingMatrix(packet[idPageMappingIndex:]),
+	}
+
+	return nil
+}
+
+func parseDemixingMatrix(in []byte) []int16 {
+	matrix := make([]int16, len(in)/2)
+	for i := range matrix {
+		offset := i * 2
+		matrix[i] = parseInt16LittleEndian(in[offset : offset+2])
+	}
+
+	return matrix
+}
+
+func parseInt16LittleEndian(in []byte) int16 {
+	return int16(in[0]) | (int16(in[1]) << 8)
+}
+
+func (o *OggReader) getPacketState() *oggPacketState {
+	if o.packetState == nil {
+		o.packetState = &oggPacketState{}
+	}
+
+	return o.packetState
+}
+
+func (o *OggReader) preparePacketQueue(pageHeader *OggPageHeader) error {
+	packetState := o.getPacketState()
+	isContinuedPacket := (pageHeader.headerType & pageHeaderTypeContinuedPacket) != 0
+	isConsecutivePage := !packetState.hasLastPacketPage || pageHeader.index == packetState.lastPacketPageIndex+1
+
+	if packetState.discardingPacket && !isContinuedPacket {
+		packetState.discardingPacket = false
+	}
+
+	if len(packetState.partialPacket) != 0 {
+		if !isContinuedPacket || !isConsecutivePage {
+			return io.ErrUnexpectedEOF
+		}
+
+		return nil
+	}
+
+	if isContinuedPacket {
+		// RFC 7845 allows the first packet on an audio page to be a continuation
+		// from data we do not have, such as when joining a live stream mid-broadcast.
+		// Skip that incomplete packet and resume at the next packet boundary.
+		packetState.discardingPacket = true
+	}
+
+	return nil
+}
+
+func (o *OggReader) enqueuePacket(pageHeader *OggPageHeader, packet []byte) {
+	headerCopy := *pageHeader
+	o.getPacketState().packetQueue = append(o.getPacketState().packetQueue, oggPacket{
+		data:   append([]byte(nil), packet...),
+		header: &headerCopy,
+	})
+}
+
+func (s *oggPacketState) finishDiscard(segmentSize byte) {
+	if segmentSize != maxPageSegmentSize {
+		s.discardingPacket = false
+	}
 }
